b8a4e72353ff06146ba86c7618ae10dec922a34c
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-09-18 19:35:29 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    
+    return (
+        <main>
+            <div className='pattern'/>
+
+            <div className="wrapper">
+                <header>
+                    <h1 className="text-3xl font-bold">
+                        Find <span className="text-gradient">Movies</span> you'll enjoy without the hassle
+                    </h1>
+                </header>
+
+                <p>Search</p>  
+            </div>
+            
+        </main>
+    )
+}
+
+
+
+export default App
