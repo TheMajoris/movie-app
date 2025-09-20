b181bac859de78f075760fe6d0084da36bce3701
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-09-20 19:33:21 +0800

    Implementation of API call and state management in React component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './components/Search.jsx'
 import './App.css'
 
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+};
+
+
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [movieList, setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchMovies = async () =>{
+        setIsLoading(true);
+        setErrorMessage('');
+
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endpoint, API_OPTIONS);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+            if(data.Response == 'False'){
+                setErrorMessage(data.Error || 'Failed to fetch movies.');
+                setMovieList([]);
+                return;
+            }
+
+            setMovieList(data.results || []);
+
+        } catch (error) {
+            console.error("Error fetching movies:", error);
+            setErrorMessage("Failed to fetch movies. Please try again later.");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, [searchTerm])
     
     return (
         <main>
@@ -15,9 +67,14 @@ const App = () => {
                     <h1 className="text-3xl font-bold">
                         Find <span className="text-gradient">Movies</span> you'll enjoy without the hassle
                     </h1>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
                 </header>
 
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                <section>
+                    <h2 className="all-movies">All Movies</h2>
+
+                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+                </section>
             </div>
             
         </main>
